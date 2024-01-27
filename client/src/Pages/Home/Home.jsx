87345f4093447de15e2b53b86ec9a9ebd9e961da
KLociJS <kisznerlorant21@gmail.com>
2024-01-27 18:37:22 +0100

    Implement landing page

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import { NAV_LINKS } from "../../Constants/Constants";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='landing-container'>
+      <h1 className='landing-header'>Secret Safe</h1>
+      <p className='landing-intro'>
+        Safeguard your secrets with SecretSafe. Share securely, read discreetly.
+      </p>
+      <Link to={NAV_LINKS.CREATE_SECRET} className='landing-cta'>
+        Get Started
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
