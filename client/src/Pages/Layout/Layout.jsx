95a0eb8df1c20981b6b5b52b1351dca27b96d078
KLociJS <kisznerlorant21@gmail.com>
2024-01-27 18:00:38 +0100

    Set layout design

--- client/src/Pages/Layout/Layout.jsx
+++ client/src/Pages/Layout/Layout.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 
+import "./Layout.css";
+
 const Layout = () => {
   return (
     <>
       <Navbar />
-      <main className='main-container'>
-        <Outlet />
-      </main>
+      <div className='main-wrapper'>
+        <main className='main-container'>
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 };
